7caa38dbd6dcf725320f07ea0244b1e41a54d96d
Josieljcc <josiel.jcc@hotmail.com>
2024-08-05 18:41:53 -0300

    feat: initial screen

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import Button from "@/components/Button";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Text, Image, View, TouchableOpacity } from "react-native";
+
+export default function IndexScreen() {
+  return (
+    <ThemedView className="flex flex-1 items-center justify-between">
+      <Image
+        source={require("@/assets/images/initialBg.jpg")}
+        className="w-full h-full absolute -z-20"
+      />
+
+      <ThemedText className="mt-10 font-bold text-zinc-50" type="link">
+        13k+ Recipes
+      </ThemedText>
+      <View className="w-full flex flex-col items-center justify-between">
+        <ThemedText className="text-5xl text-zinc-50 font-bold">
+          Let's Cooking
+        </ThemedText>
+        <ThemedText className="text-zinc-50 mb-8 text-base">
+          Find best recipes
+        </ThemedText>
+        <TouchableOpacity
+          onPress={() => console.log("pressed")}
+          className="bg-red-500 rounded-md w-[90%] mb-8 py-2 flex items-center justify-center"
+        >
+          <Text className="text-zinc-50 text-base ">Start Cooking ➤</Text>
+        </TouchableOpacity>
+      </View>
+    </ThemedView>
+  );
+}
